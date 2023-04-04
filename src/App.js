2b67840f0e120409e6ea8a0f66d43dd13e90643b
Harrison Itotia <harrisonitotia@gmail.com>
2023-04-04 12:26:00 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+export default function Header() {
+    const location = useLocation()
+    const navigate = useNavigate()
+    
+    function pathMatchRoute(route){
+        if(route === location.pathname){
+            return true
+        }
+    }
+
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-0 '>
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+            <div>
+                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="" className='h-5 cursor-pointer' onClick={() => navigate("/")}/>
+            </div>
+
+            <div>
+                <ul className='flex space-x-10'>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && "text-black border-b-red-500"}`}
+                    onClick={() => navigate("/")}
+                    >Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && "text-black border-b-red-500"}`}
+                    onClick={() => navigate("/offers")}
+                    >Offers</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/sign-in') && "text-black border-b-red-500"}`}
+                    onClick={() => navigate("/sign-in")}
+                    >Sign in</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
