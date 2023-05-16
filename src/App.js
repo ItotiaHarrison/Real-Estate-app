8b11de5d553ba1dddd427bccb3dd2f993417db59
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-16 11:15:49 +0300

    create a spinner component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Offers from "./pages/Offers";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword";
-import Header from "./components/Header";
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import PrivateRoute from './components/PrivateRoute'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+import Header from './components/Header'
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import PrivateRoute from "./components/PrivateRoute";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -16,15 +16,14 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path = "/" element={<Home/>}/>
+            <Route path="/profile" element= {<PrivateRoute/>}>
+            <Route path = "/profile" element={<Profile/>}/>
+            </Route>
+            <Route path = "/sign-in" element={<SignIn/>}/>
+            <Route path = "/sign-up" element={<SignUp/>}/>
+            <Route path = "/forgot-password" element={<ForgotPassword/>}/>
+            <Route path = "/offers" element={<Offers/>}/>
         </Routes>
       </Router>
       <ToastContainer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
     }
 
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 '>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="" className='h-5 cursor-pointer' onClick={() => navigate("/")}/>
