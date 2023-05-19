d43425bb3d5c1f96046023df9d1886bf8feec9d5
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-19 12:27:21 +0300

    complete the listingitem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -57,7 +57,7 @@ export default function Profile() {
   useEffect(()=>{
     async function fetchUserListings(){
       const listingRef = collection(db, "listings");
-      const q = query(listingRef, where("userRef", "===", auth.currentUser.uid), orderBy("timestamp","desc"))
+      const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp","desc"))
       const querySnap = await getDocs(q);
       let listings = [];
       querySnap.forEach((doc)=>{
@@ -114,8 +114,8 @@ export default function Profile() {
                 <div className='max-w-6xl px-3 mt-6 mx-auto'>
                   {!loading && listings.length > 0 && (
                     <>
-                    <h2 className='text-2xl text-center font-semibold '>My Listings</h2>
-                    <ul>
+                    <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+                    <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
                       {listings.map((listing)=>(
                         <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
                       ))}
