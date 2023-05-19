60f8542f42d8f7942389d78348666061f77cc631
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-19 13:06:13 +0300

    Add delete and edit functionality to the listing section

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Moment from 'react-moment'
 import { Link } from 'react-router-dom'
 import { MdLocationOn } from "react-icons/md"
+import { FaTrash } from "react-icons/fa"
+import { MdEdit } from "react-icons/md"
 
-export default function ListingItem({ listing, id }) {
+
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]'>
       <Link className='contents' to={`/category/${listing.type}/${id}`}>
@@ -13,18 +16,18 @@ export default function ListingItem({ listing, id }) {
         </Moment>
         <div className="w-full p-[10px]">
           <div className="flex items-center space-x-1">
-            <MdLocationOn className='h-4 w-4 text-green-600'/>
+            <MdLocationOn className='h-4 w-4 text-green-600' />
             <p className='font-semibold text-sm mb-[2px] text-gray-600 truncate'>{listing.address}</p>
           </div>
           <p className='font-semibold m-0 text-xl truncate'>{listing.name}</p>
           <p className='text-[#457b9d] mt-2 font-semibold'>
             ${listing.offer ? listing.discountedPrice
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            : listing.regularPrice
               .toString()
               .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-          }
+              : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            }
             {listing.type === "rent" && " / month"}
           </p>
           <div className="flex items-center mt-[10px] space-x-3">
@@ -37,6 +40,17 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+
+      {onDelete && (
+        <FaTrash className='absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500' onClick={() => onDelete(listing.id)} />
+
+      )}
+
+{onEdit && (
+        <MdEdit className='absolute bottom-2 right-7 h-4 cursor-pointer' onClick={() => onEdit(listing.id)} />
+
+      )}
+
     </li>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,10 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
-import {FcHome} from "react-icons/fc"
+import { FcHome } from "react-icons/fc"
 import { Link } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
 
@@ -19,23 +19,23 @@ export default function Profile() {
     email: auth.currentUser.email
   });
 
-  const {name, email} = formData
+  const { name, email } = formData
 
-  function onLogOut(){
+  function onLogOut() {
     auth.signOut()
     navigate("/")
   }
 
-  function onChange(e){
-    setFormData((prevState)=>({
+  function onChange(e) {
+    setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
 
-  async function onSubmit(){
+  async function onSubmit() {
     try {
-      if(auth.currentUser.displayName !== name){
+      if (auth.currentUser.displayName !== name) {
         //update display name in firebase auth
         await updateProfile(auth.currentUser, {
           displayName: name,
@@ -54,23 +54,36 @@ export default function Profile() {
     }
   }
 
-  useEffect(()=>{
-    async function fetchUserListings(){
+  useEffect(() => {
+    async function fetchUserListings() {
       const listingRef = collection(db, "listings");
-      const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp","desc"))
+      const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp", "desc"))
       const querySnap = await getDocs(q);
       let listings = [];
-      querySnap.forEach((doc)=>{
+      querySnap.forEach((doc) => {
         return listings.push({
           id: doc.id,
-        data: doc.data(),
+          data: doc.data(),
         });
       });
       setListings(listings);
-        setLoading(false);
+      setLoading(false);
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
+
+  async function onDelete(listingID) {
+    if(window.confirm("Are you sure you want to delete")){
+        await deleteDoc(doc(db, "listings", listingID))
+        const updatedListings = listings.filter((listing)=> listing.id !== listingID);
+        setListings(updatedListings)
+        toast.success("Successfully deleted the listing")
+    }
+  }
+
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`)
+  }
 
   return (
     <>
@@ -79,19 +92,19 @@ export default function Profile() {
 
         <div className='w-full md:w-[50%] mt-6 px-3'>
           <form>
-            <input type="text" id='name' value={name} disabled={!changeDetail} 
-            onChange={onChange}
-            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded  transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
+            <input type="text" id='name' value={name} disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded  transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`} />
 
-            <input type="email" id='email' value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded  transition ease-in-out'/>
+            <input type="email" id='email' value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded  transition ease-in-out' />
 
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
               <p className='flex items-center mb-6'>Do you want to change your name?
-                <span onClick={() =>{
+                <span onClick={() => {
                   changeDetail && onSubmit()
-                 setChangeDetail((prevState)=>!prevState);
-                }} 
-                className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
+                  setChangeDetail((prevState) => !prevState);
+                }}
+                  className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
                   {changeDetail ? 'Apply change' : 'Edit'}
                 </span>
               </p>
@@ -100,29 +113,32 @@ export default function Profile() {
             </div>
           </form>
 
-                <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
-                  <Link to="/create-listing" className='flex justify-center items-center '>
-                  <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
-                  Sell or rent your home
-                  </Link>
-                </button>
+          <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+            <Link to="/create-listing" className='flex justify-center items-center '>
+              <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2' />
+              Sell or rent your home
+            </Link>
+          </button>
 
         </div>
       </section>
 
 
-                <div className='max-w-6xl px-3 mt-6 mx-auto'>
-                  {!loading && listings.length > 0 && (
-                    <>
-                    <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
-                    <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
-                      {listings.map((listing)=>(
-                        <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
-                      ))}
-                    </ul>
-                    </>
-                  )}
-                </div>
+      <div className='max-w-6xl px-3 mt-6 mx-auto'>
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
+              {listings.map((listing) => (
+                <ListingItem key={listing.id} id={listing.id} listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
 
     </>
   )
